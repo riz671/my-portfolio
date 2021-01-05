97fc1b85eab92efef8fd3ddb1f8fa54688480349
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2021-01-04 19:30:58 -0800

    implement carousel for SDC

--- portfolio-site/pages/_app.js
+++ portfolio-site/pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import React from "react";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import "../styles/carousel.css"; //requires a loader
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
